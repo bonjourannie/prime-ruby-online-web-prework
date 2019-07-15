6fe90d36d41767a7a70c8313dbe1d066eab189d3
Annie Truex <artruex@gmail.com>
2019-07-15 17:09:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@ def prime?(num)
   if num < 0 or num == 0 or num == 1
     return false
   else
-  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
+  (2..num - 1).each {|x| return false if (num % x) == 0 }
   true
 end
